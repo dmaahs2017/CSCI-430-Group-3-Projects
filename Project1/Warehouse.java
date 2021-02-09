90f726c30bed087a0e3d7e07bd853ad5dac73235
Dalton Maahs <dalton.maahs@yourbind.com>
2021-02-09 16:34:03 -0600

    in prog

---
+++ Project1/Warehouse.java
@@ -0,0 +1,17 @@
+public class Warehouse {
+    private ClientList clientList;
+    private ProductList productList;
+    private SupplierList supplierList;
+
+    //add client
+    public Client addClient(String clientId, String fname, String lname, String address) {
+
+    }
+    //add product
+    //get product by id
+    //set product info (name quant price)
+    //get client by id
+    //set client info (fname, lname, addrs)
+    //add supplier
+    //set supplier info ()
+}
