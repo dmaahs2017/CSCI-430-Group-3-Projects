59fe4ec3b768296e6c0afb8e9cd9de47814ba7fe
jkblrnz <jkblrnz@gmail.com>
2021-02-09 16:55:25 -0600

    add get and set test

--- Project1/ClientTest.java
+++ Project1/ClientTest.java
@@ -16,6 +16,16 @@ public class ClientTest {
             System.out.println(clients.next());
         }
 
+        c1.setClientId("test007");
+        System.out.println("client id should be test007 : " + c1.getClientId());
+
+        c1.setFirstName("Alpha");
+        c1.setLastName("Bravo");
+        System.out.println("client name should be Alpha Bravo : " + c1.getFirstName() + " " + c1.getLastName());
+
+        c1.setAdress("court yard name and city");
+        System.out.println("client adress should be court yard name and city : " + c1.getAdress());
+
         System.out.println(list.toString());
     }
 }
