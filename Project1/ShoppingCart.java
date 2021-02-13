2fb92ec21a9b14e128980ab1287ce880eb3f2678
Logan K <logankrick@gmail.com>
2021-02-13 00:26:43 -0600

    Iterates over a list of shopping cart products to calculate total price

--- Project1/ShoppingCart.java
+++ Project1/ShoppingCart.java
@@ -3,18 +3,15 @@ import java.io.*;
 public class ShoppingCart implements Serializable {
   private static final long serialVersionUID = 1L;
   private List<Product> cart;
-  private double totalPrice;
 
   public ShoppingCart() {
     cart = new LinkedList<Product>();
-    totalPrice = 0;
   }
 
   // insert product(s) into the shopping cart and update total price
   public boolean insertProductToCart(Product product, int quantity) {
     product.setQuantity(quantity);
     cart.add(product);
-    totalPrice += (product.getSalePrice() * quantity);
     return true;
   }
   public Iterator<Product> getShoppingCartProducts() {
@@ -22,6 +19,14 @@ public class ShoppingCart implements Serializable {
   }
 
   public double getTotalPrice() {
+    double totalPrice = 0;
+    Iterator<Product> cartIterator = cart.iterator();
+
+    while (cartIterator.hasNext()){
+      Product p = cartIterator.next();
+      totalPrice += (p.getSalePrice() * p.getQuantity());
+    }
+    
     return totalPrice;
   }
   
--- Project1/ShoppingCartTester.java
+++ Project1/ShoppingCartTester.java
@@ -8,27 +8,27 @@ public class ShoppingCartTester {
 
     // test empty shopping cart
     System.out.println("Shopping Cart Should be empty");
-    Iterator<Product> cartProduct = shoppingCart.getShoppingCartProducts();
-    while (cartProduct.hasNext()){
-      System.out.println(cartProduct.next());
+    Iterator<Product> cartIterator = shoppingCart.getShoppingCartProducts();
+    while (cartIterator.hasNext()){
+      System.out.println(cartIterator.next());
     }
     System.out.println("Shopping Cart Total Price should be '0.0' : " + shoppingCart.getTotalPrice());
 
     // test shopping cart with 1 product added
     shoppingCart.insertProductToCart(p1,1);
     System.out.println("Shopping Cart should have 1 of product 'product1'");
-    cartProduct = shoppingCart.getShoppingCartProducts();
-    while (cartProduct.hasNext()){
-      System.out.println(cartProduct.next());
+    cartIterator = shoppingCart.getShoppingCartProducts();
+    while (cartIterator.hasNext()){
+      System.out.println(cartIterator.next());
     }
     System.out.println("Shopping Cart Total Price should be '1.99' : " + shoppingCart.getTotalPrice());
 
     // test adding another product, with a quantity of 3, to the shopping cart
     shoppingCart.insertProductToCart(p2, 3);
     System.out.println("Shopping Cart should have 2 products: 1 of 'product1', and 3 of 'product2'");
-    cartProduct = shoppingCart.getShoppingCartProducts();
-    while (cartProduct.hasNext()){
-      System.out.println(cartProduct.next());
+    cartIterator = shoppingCart.getShoppingCartProducts();
+    while (cartIterator.hasNext()){
+      System.out.println(cartIterator.next());
     }
     System.out.println("Shopping Cart Total Price should be '16.96' : " + shoppingCart.getTotalPrice());
   }
