2294a22ad1f76a43cb49ea775972481d87a5f682
Logan K <logankrick@gmail.com>
2021-02-26 17:33:20 -0600

    Created Order, OrderList, OrderTester and integrated into UI and Warehouse

---
+++ Project1/Order.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class Order implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private Client client;
+  private String orderId;
+  private ShoppingCart orderedCart;  
+
+  public Order(String orderId, Client client) {
+    this.orderId = orderId;
+    this.client = client;
+    this.orderedCart = client.getShoppingCart();
+  }
+
+  public Iterator<Product> getOrderProducts() {
+    return orderedCart.getShoppingCartProducts();
+  }
+
+  public Client getOrderClient() {
+    return client;
+  }
+
+  public Boolean equals(String id) {
+    return this.orderId.equals(id);
+  }
+
+  public String toString() {
+      return "orderId " + orderId + " clientId " + client.getClientId() + " ordered_products " + orderedCart.toString();
+  }
+}
