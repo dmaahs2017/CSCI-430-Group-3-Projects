65cd1ad40c148e58a9352334a14c30687997ab10
Logan K <logankrick@gmail.com>
2021-03-07 17:34:44 -0600

    Added Inventory and InventoryTester

---
+++ Project1/Inventory.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+public class Inventory implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private List<Product> inventoryList = new LinkedList<Product>();
+  private static Inventory inventory;
+  private Inventory() {
+  }
+  public static Inventory instance() {
+    if (inventory == null) {
+      return (inventory = new Inventory());
+    } else {
+      return inventory;
+    }
+  }
+
+  public boolean addToInventory(Product product, int quantity) {
+    product.setQuantity(quantity);
+    inventoryList.add(product);
+    return true;
+  }
+
+  public Iterator<Product> getInventory(){
+     return inventoryList.iterator();
+  }
+  
+  private void writeObject(java.io.ObjectOutputStream output) {
+    try {
+      output.defaultWriteObject();
+      output.writeObject(inventory);
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+  private void readObject(java.io.ObjectInputStream input) {
+    try {
+      if (inventory != null) {
+        return;
+      } else {
+        input.defaultReadObject();
+        if (inventory == null) {
+          inventory = (Inventory) input.readObject();
+        } else {
+          input.readObject();
+        }
+      }
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    } catch(ClassNotFoundException cnfe) {
+      cnfe.printStackTrace();
+    }
+  }
+  public String toString() {
+    return inventoryList.toString();
+  }
+}
