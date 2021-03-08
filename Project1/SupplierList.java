d7f1f5e2b4bf52fa30194d2e33bb21e0c90bbf75
Logan K <logankrick@gmail.com>
2021-03-07 21:09:32 -0600

    Implemented Warehouse Serialization, save and load the Warehouse

--- Project1/SupplierList.java
+++ Project1/SupplierList.java
@@ -24,6 +24,33 @@ public class SupplierList implements Serializable {
         return suppliers.iterator();
     }
 
+    private void writeObject(java.io.ObjectOutputStream output) {
+        try {
+          output.defaultWriteObject();
+          output.writeObject(supplierList);
+        } catch(IOException ioe) {
+          ioe.printStackTrace();
+        }
+      }
+      private void readObject(java.io.ObjectInputStream input) {
+        try {
+          if (supplierList != null) {
+            return;
+          } else {
+            input.defaultReadObject();
+            if (supplierList == null) {
+                supplierList = (SupplierList) input.readObject();
+            } else {
+                input.readObject();
+            }
+          }
+        } catch(IOException ioe) {
+          ioe.printStackTrace();
+        } catch(ClassNotFoundException cnfe) {
+          cnfe.printStackTrace();
+        }
+      }
+
     public String toString() {
         return suppliers.toString();
     }
